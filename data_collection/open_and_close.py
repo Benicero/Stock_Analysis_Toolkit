71d8539a3ddd0e30a27341f4391c8ec7dc457dc0
Benjamin Lucero <Benjaminlucero01@yahoo.com>
2019-06-30 21:57:37 -0400

    Add files via upload

---
+++ data_collection/open_and_close.py
@@ -0,0 +1,24 @@
+import requests
+import pandas as pd 
+import pandas_datareader as pdr # gives an error but still works ?
+from datetime import datetime
+
+"""
+***THIS METHOD IS TECHNICALLY DEPRECATED SO WE SHOULD EVENTUALLY USE ANOTHER ONE***
+
+---return pandas dataframe---
+Fields: 
+['High' 'Low' 'Open' 'Close' 'Volume' 'Adj Close']
+"""
+
+# variable specifications
+symbol = 'IBM'
+start = datetime(2000, 1, 1)
+end = datetime(2012, 1, 1)
+
+# access the data and return pandas dataframe
+ibm = pdr.get_data_yahoo(symbols=symbol, start=start, end=end)
+
+# write collected data to csv file
+ibm.csvwrt
+
