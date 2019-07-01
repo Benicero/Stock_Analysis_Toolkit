adbed6906405054222ad784aa6fe261fec66fb88
Julian Rice <julianrice@dotcomjungle.com>
2019-07-01 00:54:17 -0700

    familiarizing with scrapy, scraping NASDAQ

--- data_collection/open_and_close.py
+++ data_collection/open_and_close.py
@@ -9,12 +9,22 @@ from datetime import datetime
 ---return pandas dataframe---
 Fields: 
 ['High' 'Low' 'Open' 'Close' 'Volume' 'Adj Close']
+
+it looks like this data is 2 days behind, but thats ok for now
 """
 
 # variable specifications
 symbol = 'IBM'
 start = datetime(2000, 1, 1)
-end = datetime(2012, 1, 1)
+end = datetime.today()
 
 # access the data and return pandas dataframe
 ibm = pdr.get_data_yahoo(symbols=symbol, start=start, end=end)
+
+
+
+
+print(ibm[-10:])
+
+ibm_max = ibm.loc[ibm['High'].idxmax()]
+print(ibm_max)
---
+++ data_collection/scrapy_test_1.py
@@ -0,0 +1,49 @@
+"""
+
+run this file using:
+    scrapy runspider scrapy_test_1.py -o <output filename>.json
+
+
+"""
+
+import scrapy
+import json
+
+
+class QuotesSpider(scrapy.Spider):
+    AUTOTHROTTLE_ENABLED = True
+
+    name = 'nasdaq_scrape'
+    # load all 40,000+ companies from the nasdaq
+    start_urls = [
+        'https://www.nasdaq.com/screening/companies-by-industry.aspx?exchange=NASDAQ&pagesize=50000'
+    ]
+
+    # get data for each, gets written to json file by scrapy
+    def parse(self, response):
+        for table in response.css('#CompanylistResults'):
+            # alternating rows in the table have the stuff we actually want
+            data_we_want = False
+            for row in table.css('tr'):
+                if data_we_want:
+                    data_we_want = False
+                    # get the data
+                    data = {
+                        'name'      : row.css(':nth-child(1) > a::text').get(),
+                        'code'      : row.css(':nth-child(2) > h3 > a::text').get(),
+                        'price'     : row.css(':nth-child(3)::text').get(),
+                        'country'   : row.css(':nth-child(4)::text').get(),
+                        'sector'    : row.css(':nth-child(6)::text').get(),
+                    }
+                    # clean tabs and whitespace
+                    for key, val in data.items():
+                        try:
+                            data[key] = val.strip()
+                        except AttributeError:
+                            pass
+                    yield data
+                else:
+                    data_we_want = True
+
+
+
